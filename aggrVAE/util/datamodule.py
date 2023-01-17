0b566c1dd0361d8e06d6dc1116a8c00ae82a9d82
Andrew Parry <2393265p@student.gla.ac.uk>
2023-01-17 19:47:11 +0000

    Split up datamodules

--- aggrVAE/util/datamodule.py
+++ aggrVAE/util/datamodule.py
@@ -12,56 +12,84 @@ from torchvision import transforms
 
 import numpy as np
 
-mnist_transform=transforms.Compose([transforms.ToTensor(),
-                            emnist_normalization('mnist')
-                            ])
-cifar_transform=transforms.Compose([transforms.ToTensor(),
-                            cifar10_normalization()
-                            ])
-
-
-META = {
-    'CIFAR10' : (CIFAR10, cifar_transform, 32, 3, 10),
-    'MNIST' : (MNIST, mnist_transform, 28, 1, 10)
-}
-
-class ImageDataModule(pl.LightningDataModule):
-    def __init__(self, train_dir, name, batch_size, num_workers, dataset_root=None) -> None:
-        super(ImageDataModule).__init__()
+class AggrMNISTDataModule(pl.LightningDataModule):
+    def __init__(self, train_dir, batch_size, num_workers, dataset_root=None) -> None:
+        super(AggrMNISTDataModule).__init__()
         self.prepare_data_per_node = False
         self._log_hyperparams = False
         root = dataset_root if dataset_root else '/'
+        self.transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(),
+                            emnist_normalization('mnist')
+                            ])
 
         self.source = train_dir
-        self.sink = os.path.join(root, name)
-        self.name = name
-        self.height = META[name][2]
-        self.channels = META[name][3]
-        self.classes = META[name][4]
+        self.sink = os.path.join(root, 'MNIST')
+        self.name = 'MNIST'
+        self.height = 28
+        self.channels = 1
+        self.classes = 10
 
         self.batch = batch_size
         self.workers = num_workers
 
     def prepare_data(self):
-        pass 
+        MNIST(self.sink, train=False, download=True, transform=self.transform)
 
     def setup(self, stage: Optional[str] = None): # Will make this cleaner later
         if stage == "fit" or stage is None:
             with open(self.source, 'rb') as f:
                 x, y, _ = pickle.load(f)
-                if self.name == 'MNIST':
-                    x = np.expand_dims(x, axis=1)
-                else:
-                    x = np.einsum('ijkl->iljk', x)
-                print(x.shape)
-                ds, transform, _, _, _ = META[self.name]
-            self.train, self.validate = TensorDataset(torch.Tensor(x), torch.Tensor(y)), ds(self.sink, train=False, download=True, transform=transform)
+            x = np.expand_dims(x, axis=1)
+
+            self.train, self.validate = TensorDataset(torch.Tensor(x), torch.Tensor(y)), MNIST(self.sink, train=False, download=True, transform=self.transform)
 
         # Assign test dataset for use in dataloader(s)
         if stage == "test" or stage is None:
-            ds, transform, _, _, _ = META[self.name]
+            self.test = MNIST(self.sink, train=False, download=True, transform=self.transform)
+            
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=self.batch, num_workers=self.workers)
 
-            self.test = ds(self.sink, train=False, download=True, transform=transform)
+    def val_dataloader(self):
+        return DataLoader(self.validate, batch_size=self.batch, num_workers=self.workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.test, batch_size=self.batch, num_workers=self.workers)
+
+class AggrCIFAR10DataModule(pl.LightningDataModule):
+    def __init__(self, train_dir, batch_size, num_workers, dataset_root=None) -> None:
+        super(AggrCIFAR10DataModule).__init__()
+        self.prepare_data_per_node = False
+        self._log_hyperparams = False
+        root = dataset_root if dataset_root else '/'
+        self.transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(),
+                            cifar10_normalization()
+                            ])
+
+        self.source = train_dir
+        self.sink = os.path.join(root, 'CIFAR10')
+        self.name = 'CIFAR10'
+        self.height = 32
+        self.channels = 3
+        self.classes = 10
+
+        self.batch = batch_size
+        self.workers = num_workers
+
+    def prepare_data(self):
+        CIFAR10(self.sink, train=False, download=True, transform=self.transform)
+
+    def setup(self, stage: Optional[str] = None): # Will make this cleaner later
+        if stage == "fit" or stage is None:
+            with open(self.source, 'rb') as f:
+                x, y, _ = pickle.load(f)
+            x = np.einsum('ijkl->iljk', x)
+
+            self.train, self.validate = TensorDataset(torch.Tensor(x), torch.Tensor(y)), CIFAR10(self.sink, train=False, download=True, transform=self.transform)
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test" or stage is None:
+            self.test = CIFAR10(self.sink, train=False, download=True, transform=self.transform)
             
     def train_dataloader(self):
         return DataLoader(self.train, batch_size=self.batch, num_workers=self.workers)
