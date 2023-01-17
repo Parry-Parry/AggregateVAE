45db36862d9ac8b2668cad90791795fe1d07f594
Andrew Parry <2393265p@student.gla.ac.uk>
2023-01-17 14:08:33 +0000

    Adding logs

--- aggrVAE/scripts/train_image.py
+++ aggrVAE/scripts/train_image.py
@@ -1,6 +1,8 @@
 from argparse import ArgumentParser
 import os
+import pickle
 import pytorch_lightning as pl
+from pytorch_lightning.loggers import CSVLogger
 import torch
 from aggrVAE.util.datamodule import ImageDataModule
 from aggrVAE.image_model.vae import classifier_head, VAEclassifier
@@ -11,13 +13,11 @@ parser = ArgumentParser()
 
 parser.add_argument('-data_source', type=str)
 parser.add_argument('-data_sink', type=str)
-parser.add_argument('-model_sink', type=str)
+parser.add_argument('-log_dir', type=str)
 parser.add_argument('-ds', type=str)
 parser.add_argument('--epochs', type=int)
 parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--num_workers', type=int, default=4)
-parser.add_argument('--n_class', type=int, default=10)
-parser.add_argument('--in_height', type=int, default=32)
 parser.add_argument('--K', type=int, default=50)
 parser.add_argument('--seed', type=int, default=8008)
 parser.add_argument('--heads', type=int, default=1)
@@ -28,7 +28,18 @@ CONV = [32, 32, 64, 64]
 LINEAR = [512, 256, 128]
 
 def main(args):
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(dirpath=args.model_sink, save_weights_only=True, every_n_epochs=10)
+
+    assert os.path.exists(args.log_dir)
+    root_path = os.path.join(args.log_dir, f"{args.ds}.{args.K}.{args.heads}")
+    if not os.path.exists(root_path):
+        os.mkdir(root_path)
+        chkpt = os.path.join(root_path, 'checkpoints')
+        os.mkdir(chkpt)
+        history = os.path.join(root_path, 'history')
+        os.mkdir(history)
+
+    logger = CSVLogger(root_path, name=f"{args.ds}.{args.K}.{args.heads}")
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(dirpath=os.path.join(root_path, 'checkpoints'), save_weights_only=True, every_n_epochs=10)
 
     data_name = f'{args.ds}{args.K}{args.seed}.pkl'
     data = ImageDataModule(os.path.join(args.data_source, data_name), args.ds, args.batch_size, args.data_sink)
@@ -40,8 +51,13 @@ def main(args):
         head = classifier_head(CONV, LINEAR, data.height, data.channels, data.classes)
         model = VAEclassifier(head, 512, args.latent, data.classes, data.height, data.channels)
 
-    trainer = pl.Trainer(max_epochs=args.epochs, accelerator='auto', devices=1 if torch.cuda.is_available() else None, accumulate_grad_batches=args.accum, callbacks=[checkpoint_callback])
+    trainer = pl.Trainer(max_epochs=args.epochs, logger=logger, accelerator='auto', devices=1 if torch.cuda.is_available() else None, accumulate_grad_batches=args.accum, callbacks=[checkpoint_callback])
     trainer.fit(model, data)
+    hist = trainer.test(data)
+
+    with open(os.path.join(root_path, 'history', f'history.{args.ds}.{args.K}.{args.heads}.pkl'), 'wb') as f:
+        pickle.dump(f, hist)
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
