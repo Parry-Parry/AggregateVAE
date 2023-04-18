609831da8d767f27a71d1b54b709b5d8baa3dd1a
Andrew Parry <2393265p@student.gla.ac.uk>
2023-04-18 18:15:48 +0100

    Adding new files

---
+++ aggrVAE/kmeans.py
@@ -0,0 +1,7 @@
+from fire import Fire
+
+def main(dataset : str, outdir : str, K : int = 50):
+    pass
+
+if __name__ == '__main__':
+    Fire(main)
---
+++ aggrVAE/util.py
@@ -0,0 +1,3 @@
+
+def load_norm(dataset : str):
+    pass
