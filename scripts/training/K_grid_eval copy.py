edd29c802fa2fa3d53734b883f33b0be96ac583b
Andrew Parry <2393265p@student.gla.ac.uk>
2023-05-05 15:05:04 +0100

    just do 50 epochs for now

--- scripts/training/K_grid_eval copy.py
+++ scripts/training/K_grid_eval copy.py
@@ -3,14 +3,15 @@ import os
 from os.path import join
 
 HEADS = [1, 3, 5, 10, 20]
-EPOCHS = [10, 20, 30, 40, 50]
+EPOCHS = [50]
 EPSILON = [0., 0.0001, 0.005, 0.01, 0.05, 0.1, 0.5]
+K = [50, 100, 200, 500, 1000]
 
 def main(script : str, 
          dataset : str,
          datastore : str, 
          outstore : str, 
-         trainstore : str = None,  
+         trainstore : str, 
          batch_size : int = 8, 
          latent_dim : int = 10, 
          cat_dim : int = 10, 
@@ -32,26 +33,27 @@ def main(script : str,
     
     vae_str = 'vae' if vae else 'classifier'
 
-    if trainstore: args.extend(['--trainstore', trainstore])
     if vae: args.append('--vae')
     if p: args.extend(['--p', str(p)])
     
     for epoch in EPOCHS:
         for head in HEADS:
-            tmp_args = args.copy()
-            tmp_args.extend(['--epochs', str(epoch)])
-            tmp_args.extend(['--num_heads', str(head)])
-            if eps is not None: 
-                tmp_args.extend(['--epsilon', str(eps)])
-                tmp_args.extend(['--outstore', join(outstore, f'{dataset}-{epoch}-{head}-{eps}-{vae_str}')])
-                print(' '.join(tmp_args))
-                os.system(' '.join(tmp_args))
-            else: 
-                for epsilon in EPSILON:
-                    eps_args = tmp_args.copy()
-                    eps_args.extend(['--outstore', join(outstore, f'{dataset}-{epoch}-{head}-{epsilon}-{vae_str}')])
-                    eps_args.extend(['--epsilon', str(epsilon)])
-                    print(' '.join(eps_args))
-                    os.system(' '.join(eps_args))
+            for k in K:
+                args.extend(['--trainstore', join(trainstore, f'{dataset}.{k}.npy.npz')])
+                tmp_args = args.copy()
+                tmp_args.extend(['--epochs', str(epoch)])
+                tmp_args.extend(['--num_heads', str(head)])
+                if eps is not None: 
+                    tmp_args.extend(['--epsilon', str(eps)])
+                    tmp_args.extend(['--outstore', join(outstore, f'{dataset}-{epoch}-{head}-{eps}-{vae_str}')])
+                    print(' '.join(tmp_args))
+                    os.system(' '.join(tmp_args))
+                else: 
+                    for epsilon in EPSILON:
+                        eps_args = tmp_args.copy()
+                        eps_args.extend(['--outstore', join(outstore, f'{dataset}-{epoch}-{head}-{epsilon}-{vae_str}')])
+                        eps_args.extend(['--epsilon', str(epsilon)])
+                        print(' '.join(eps_args))
+                        os.system(' '.join(eps_args))
 if __name__ == '__main__':
     Fire(main)
