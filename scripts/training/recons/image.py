8537eadeb2f3a1c6efd1aa76a6bc59be75c54e41
Andrew Parry <2393265p@student.gla.ac.uk>
2023-05-23 10:28:38 +0100

    wild

--- scripts/training/recons/image.py
+++ scripts/training/recons/image.py
@@ -5,6 +5,7 @@ import multiprocessing as mp
 import torch
 import torchmetrics
 from os.path import join
+from tqdm.auto import tqdm
 from aggrVAE.models.classifier import SequentialClassifier, EnsembleClassifier
 from aggrVAE.models.modules import ConvEncoder
 from aggrVAE.datamodule import ReconsCIFAR10DataModule, ReconsMNISTDataModule
@@ -37,6 +38,9 @@ def main(dataset : str,
 
     ds = ds_funcs[dataset](trainstore, batch_size, cpus, datastore, epsilon, num_heads)
 
+    device = 'cuda' if gpus > 0 else 'cpu'
+    device = torch.device(device)
+
     ds.prepare_data()
     ds.setup()
 
@@ -61,41 +65,38 @@ def main(dataset : str,
                                     enc_dim=enc_dim,
                                     latent_dim=cat_dim*latent_dim)
     
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
-    if gpus > 0: model = model.cuda()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    model = model.to(device)
 
     train = ds.train_dataloader()
     val = ds.val_dataloader()
     test = ds.test_dataloader()
 
-    if gpus > 0: train = train.cuda()
-
-    for epoch in range(epochs):
+    for epoch in tqdm(range(epochs)):
         log = Log(epoch, {}, {})
         error = []
         for batch_idx, batch in enumerate(train):
             optimizer.zero_grad() 
             loss = model.training_step(batch, batch_idx)
-            error.append(loss)
+            error.append({str(k) : v.detach().cpu().numpy().item() for k, v in loss.items()})
             loss = loss['loss']
             loss.backward()
             optimizer.step()
-        validation = model.validation_step(val)
-        print(f'Epoch {epoch} : {validation}')
 
         validation = model.validation_step(val, metrics)
         logging.info(f'Epoch {epoch} : {validation}')
-        log.loss.update({epoch : {k : sum([e[k] for e in error])/len(error) for k in error[0].keys()}})
+        log.loss.update({epoch : {str(k) : sum([e[k] for e in error])/len(error) for k in error[0].keys()}})
         loss = log.loss[epoch]['loss']
         logging.info(f'Epoch {epoch} : Loss: {loss}')
-        log.val_metrics.update(validation)
+        log.val_metrics.update({str(k) : v.detach().cpu().numpy().item() for k, v in validation.items()})
         store.logs.append(log)
     
     test = model.validation_step(test, metrics)
-    store.test_metrics.extend(test)
+    store.test_metrics.update({str(k) : v.detach().cpu().numpy().item() for k, v in test.items()})
 
-    torch.save(model.state_dict(), join(outstore, 'models', f'{dataset}.{epochs}.model.{num_heads}.recons.pt'))
-    dump_logs(store, outstore)   
+    vae = 'vae' if vae else 'std'
+    torch.save(model.state_dict(), join(outstore, 'models', f'{dataset}.{epochs}.model.{num_heads}.{vae}.pt'))
+    dump_logs(store, outstore)
 
 if __name__ == '__main__':
     Fire(main)
