101b7c46dcb88df5abd4ba658b93d4b1e6db952c
Andrew Parry <2393265p@student.gla.ac.uk>
2023-05-23 10:40:37 +0100

    all goosed up

--- scripts/training/run_all_image.py
+++ scripts/training/run_all_image.py
@@ -2,11 +2,13 @@ from fire import Fire
 import os
 
 def main():
-    os.system('python AggregateVAE/scripts/training/k_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset mnist --datastore ds/dump --outstore train/image/aggr --trainstore ds/mnist --batch_size 16 --gpus 1')
-    os.system('python AggregateVAE/scripts/training/k_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset cifar10 --datastore ds/dump --outstore train/image/aggr --trainstore ds/cifar10 --batch_size 16 --gpus 1')
-    os.system('python AggregateVAE/scripts/training/k_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset mnist --datastore ds/dump --outstore train/image/aggr --trainstore ds/mnist --batch_size 16 --gpus 1 --vae')
-    os.system('python AggregateVAE/scripts/training/k_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset cifar10 --datastore ds/dump --outstore train/image/aggr --trainstore ds/cifar10 --batch_size 16 --gpus 1 --vae')
-    os.system('python AggregateVAE/scripts/training/recons_grid_eval.py --script AggregateVAE/scripts/training/recons/image.py --dataset mnist --datastore ds/dump --outstore train/image/aggr --trainstore ds/mnist --batch_size 16 --gpus 1')
-    os.system('python AggregateVAE/scripts/training/recons_grid_eval.py --script AggregateVAE/scripts/training/recons/image.py --dataset cifar10 --datastore ds/dump --outstore train/image/aggr --trainstore ds/cifar10 --batch_size 16 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/std_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset mnist --datastore ds/dump --outstore train/image/baseline --batch_size 32 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/std_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset cifar10 --datastore ds/dump --outstore train/image/baseline --batch_size 32 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/k_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset mnist --datastore ds/dump --outstore train/image/aggr --trainstore ds/mnist --batch_size 32 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/k_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset cifar10 --datastore ds/dump --outstore train/image/aggr --trainstore ds/cifar10 --batch_size 32 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/k_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset mnist --datastore ds/dump --outstore train/image/aggr --trainstore ds/mnist --batch_size 32 --gpus 1 --vae')
+    os.system('python AggregateVAE/scripts/training/k_grid_eval.py --script AggregateVAE/scripts/training/std/image.py --dataset cifar10 --datastore ds/dump --outstore train/image/aggr --trainstore ds/cifar10 --batch_size 32 --gpus 1 --vae')
+    os.system('python AggregateVAE/scripts/training/recons_grid_eval.py --script AggregateVAE/scripts/training/recons/image.py --dataset mnist --datastore ds/dump --outstore train/image/aggr --trainstore ds/mnist --batch_size 32 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/recons_grid_eval.py --script AggregateVAE/scripts/training/recons/image.py --dataset cifar10 --datastore ds/dump --outstore train/image/aggr --trainstore ds/cifar10 --batch_size 32 --gpus 1')
 if __name__ == '__main__':
     Fire(main)
