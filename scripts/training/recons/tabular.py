43ce7f7d84cb7eb46b8c16ea7920732682a85b68
Andrew Parry <2393265p@student.gla.ac.uk>
2023-06-01 10:05:47 +0100

    Sorted mismatch

--- scripts/training/recons/tabular.py
+++ scripts/training/recons/tabular.py
@@ -1,8 +1,9 @@
+import logging
 import os
 from fire import Fire
 import multiprocessing as mp
 import torch
-import torchmetrics as tm
+import torchmetrics 
 from os.path import join
 from aggrVAE.models.classifier import SequentialClassifier, EnsembleClassifier
 from aggrVAE.models.modules import DenseEncoder
@@ -35,10 +36,10 @@ def main(dataset : str,
     ds.prepare_data()
     ds.setup()
 
-    metrics = [tm.Accuracy(task='multiclass', num_classes=ds.classes), 
-               tm.F1(task='multiclass', num_classes=ds.classes), 
-               tm.Precision(task='multiclass', num_classes=ds.classes), 
-               tm.Recall(task='multiclass', num_classes=ds.classes)]
+    metrics = {'accuracy' : torchmetrics.Accuracy(task="multiclass", num_classes=ds.classes), 
+           'f1' : torchmetrics.F1Score(task="multiclass", num_classes=ds.classes),
+           'precision' : torchmetrics.Precision(task="multiclass", num_classes=ds.classes),
+           'recall' : torchmetrics.Recall(task="multiclass", num_classes=ds.classes)}
 
     encoder = DenseEncoder(ds.features, ENCODER_STACK, latent_dim=enc_dim)
     head = callable_head(latent_dim * cat_dim, STACK, ds.classes)
@@ -72,18 +73,21 @@ def main(dataset : str,
         for batch_idx, batch in enumerate(train):
             optimizer.zero_grad() 
             loss = model.training_step(batch, batch_idx)
-            error.append(loss)
-            loss['loss'].backward()
+            error.append({str(k) : v.detach().cpu().numpy().item() for k, v in loss.items()})
+            loss = loss['loss']
+            loss.backward()
             optimizer.step()
-        validation = model.validation_step(val)
-        print(f'Epoch {epoch} : {validation}')
 
-        log.loss.extend({k : sum([e[k] for e in error])/len(error) for k in error[0].keys()})
-        log.val_metrics.extend(validation, metrics)
+        validation = model.validation_step(val, metrics)
+        logging.info(f'Epoch {epoch} : {validation}')
+        log.loss.update({epoch : {str(k) : sum([e[k] for e in error])/len(error) for k in error[0].keys()}})
+        loss = log.loss[epoch]['loss']
+        logging.info(f'Epoch {epoch} : Loss: {loss}')
+        log.val_metrics.update({str(k) : v.detach().cpu().numpy().item() for k, v in validation.items()})
         store.logs.append(log)
     
     test = model.validation_step(test, metrics)
-    store.test_metrics.extend(test)
+    store.test_metrics.update({str(k) : v.detach().cpu().numpy().item() for k, v in test.items()})
 
     torch.save(model.state_dict(), join(outstore, 'models', f'recons-{dataset}.{epochs}.model.{num_heads}.classifier.pt'))
     dump_logs(store, outstore)
--- scripts/training/std/tabular.py
+++ scripts/training/std/tabular.py
@@ -1,8 +1,9 @@
+import logging
 import os
 from fire import Fire
 import multiprocessing as mp
 import torch
-import torchmetrics as tm
+import torchmetrics 
 from os.path import join
 from aggrVAE.models.vae import SequentialVAE, EnsembleVAE
 from aggrVAE.models.classifier import SequentialClassifier, EnsembleClassifier
@@ -41,10 +42,10 @@ def main(dataset : str,
     ds.prepare_data()
     ds.setup()
 
-    metrics = [tm.Accuracy(task='multiclass', num_classes=ds.classes), 
-               tm.F1(task='multiclass', num_classes=ds.classes), 
-               tm.Precision(task='multiclass', num_classes=ds.classes), 
-               tm.Recall(task='multiclass', num_classes=ds.classes)]
+    metrics = {'accuracy' : torchmetrics.Accuracy(task="multiclass", num_classes=ds.classes), 
+           'f1' : torchmetrics.F1Score(task="multiclass", num_classes=ds.classes),
+           'precision' : torchmetrics.Precision(task="multiclass", num_classes=ds.classes),
+           'recall' : torchmetrics.Recall(task="multiclass", num_classes=ds.classes)}
 
     encoder = DenseEncoder(ds.features, ENCODER_STACK, latent_dim=enc_dim)
     head = callable_head(latent_dim * cat_dim, STACK, ds.classes)
@@ -84,7 +85,7 @@ def main(dataset : str,
                                          latent_dim=cat_dim*latent_dim,
                                          epsilon=epsilon)
     
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
     if gpus > 0: model = model.cuda()
 
     train = ds.train_dataloader()
@@ -95,21 +96,23 @@ def main(dataset : str,
         log = Log(epoch, {}, {})
         error = []
         for batch_idx, batch in enumerate(train):
-            if gpus: batch = batch.cuda()
             optimizer.zero_grad() 
             loss = model.training_step(batch, batch_idx)
-            error.append(loss)
-            loss['loss'].backward()
+            error.append({str(k) : v.detach().cpu().numpy().item() for k, v in loss.items()})
+            loss = loss['loss']
+            loss.backward()
             optimizer.step()
-        validation = model.validation_step(val)
-        print(f'Epoch {epoch} : {validation}')
 
-        log.loss.extend({k : sum([e[k] for e in error])/len(error) for k in error[0].keys()})
-        log.val_metrics.extend(validation, metrics)
+        validation = model.validation_step(val, metrics)
+        logging.info(f'Epoch {epoch} : {validation}')
+        log.loss.update({epoch : {str(k) : sum([e[k] for e in error])/len(error) for k in error[0].keys()}})
+        loss = log.loss[epoch]['loss']
+        logging.info(f'Epoch {epoch} : Loss: {loss}')
+        log.val_metrics.update({str(k) : v.detach().cpu().numpy().item() for k, v in validation.items()})
         store.logs.append(log)
     
     test = model.validation_step(test, metrics)
-    store.test_metrics.extend(test)
+    store.test_metrics.update({str(k) : v.detach().cpu().numpy().item() for k, v in test.items()})
 
     vae = 'vae' if vae else 'std'
     torch.save(model.state_dict(), join(outstore, 'models', f'{dataset}.{epochs}.model.{num_heads}.{vae}.pt'))
