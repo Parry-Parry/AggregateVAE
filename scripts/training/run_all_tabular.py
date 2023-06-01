f09879bfb27080021377e1faeac69029d339b415
Andrew Parry <2393265p@student.gla.ac.uk>
2023-06-01 18:51:40 +0100

    sjfwekjfn

--- scripts/training/run_all_tabular.py
+++ scripts/training/run_all_tabular.py
@@ -3,8 +3,7 @@ import os
 
 def main():
     os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankbalancejob --datastore ds/bank/balancejob --outstore train/bank/aggr --batch_size 8 --gpus 1')
-    os.system('python AggregateVAE/scripts/training/std_grid_eval.py --script AggregateVAE/scripts/training/std/tabular.py --dataset bankjob --datastore ds/bankjob --outstore train/bank/aggr --batch_size 8 --gpus 1')
     os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --datasetjob bankjob --datastore ds/bankjob --outstore train/bank/aggr --batch_size 8 --gpus 1')
-    os.system('python AggregateVAE/scripts/training/std_grid_eval.py --script AggregateVAE/scripts/training/std/tabular.py --dataset bank --datastore ds/bank/std --outstore train/bank/baseline --batch_size 128 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --datasetjob bankbalance --datastore ds/bankbalance --outstore train/bank/aggr --batch_size 8 --gpus 1')
 if __name__ == '__main__':
     Fire(main)
--- scripts/training/run_all_tabularall.py
+++ scripts/training/run_all_tabularall.py
@@ -7,10 +7,10 @@ def main():
     os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankbalance --datastore ds/bank/balance --outstore train/bank/ --batch_size 8 --gpus 1')
     os.system('python AggregateVAE/scripts/training/std_grid_eval.py --script AggregateVAE/scripts/training/std/tabular.py --dataset bankbalancejob --datastore ds/bank/balancejob --outstore train/bank/aggr --batch_size 8 --gpus 1')
     os.system('python AggregateVAE/scripts/training/std_grid_eval.py --script AggregateVAE/scripts/training/std/tabular.py --dataset bankbalancejob --datastore ds/bank/balancejob --outstore train/bank/aggr --batch_size 8 --gpus 1 --vae')
-    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --datasetjob bankbalancejob --datastore ds/bank/balancejob --outstore train/bank/aggr --batch_size 8 --gpus 1')
+    os.system('##python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankbalancejob --datastore ds/bank/balancejob --outstore train/bank/aggr --batch_size 8 --gpus 1')
     os.system('python AggregateVAE/scripts/training/std_grid_eval.py --script AggregateVAE/scripts/training/std/tabular.py --dataset bankjob --datastore ds/bank/job --outstore train/bank/aggr --batch_size 8 --gpus 1')
     os.system('python AggregateVAE/scripts/training/std_grid_eval.py --script AggregateVAE/scripts/training/std/tabular.py --dataset bankjob --datastore ds/bank/job --outstore train/bank/aggr --batch_size 8 --gpus 1 --vae')
-    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --datasetjob bankjob --datastore ds/bank/job --outstore train/bank/aggr --batch_size 8 --gpus 1')
+    os.system('##python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankjob --datastore ds/bank/job --outstore train/bank/aggr --batch_size 8 --gpus 1')
     os.system('python AggregateVAE/scripts/training/std_grid_eval.py --script AggregateVAE/scripts/training/std/tabular.py --dataset bank --datastore ds/bank/std --outstore train/bank/baseline --batch_size 8 --gpus 1')
 if __name__ == '__main__':
     Fire(main)
