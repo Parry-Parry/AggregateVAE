20ebdd8ffeacda8ac941d1b2dba60e33b550870e
Andrew Parry <2393265p@student.gla.ac.uk>
2023-06-02 10:40:04 +0100

    script edit

--- scripts/training/run_all_tabular.py
+++ scripts/training/run_all_tabular.py
@@ -2,8 +2,7 @@ from fire import Fire
 import os
 
 def main():
-    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankbalancejob --datastore ds/bank/balancejob --outstore train/bank/aggr --batch_size 8 --gpus 1')
-    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --datasetjob bankjob --datastore ds/bankjob --outstore train/bank/aggr --batch_size 8 --gpus 1')
-    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --datasetjob bankbalance --datastore ds/bankbalance --outstore train/bank/aggr --batch_size 8 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankjob --datastore ds/bankjob --outstore train/bank/aggr --batch_size 8 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankbalance --datastore ds/bankbalance --outstore train/bank/aggr --batch_size 8 --gpus 1')
 if __name__ == '__main__':
     Fire(main)
