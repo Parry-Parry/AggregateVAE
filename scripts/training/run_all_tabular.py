fd20e03ced6833ba0f78609b55069030d0629644
Andrew Parry <2393265p@student.gla.ac.uk>
2023-06-02 10:57:39 +0100

    S

--- scripts/training/run_all_tabular.py
+++ scripts/training/run_all_tabular.py
@@ -2,7 +2,7 @@ from fire import Fire
 import os
 
 def main():
-    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankjob --datastore ds/bankjob --outstore train/bank/aggr --batch_size 8 --gpus 1')
-    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankbalance --datastore ds/bankbalance --outstore train/bank/aggr --batch_size 8 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankjob --datastore ds/bank/job --outstore train/bank/aggr --batch_size 8 --gpus 1')
+    os.system('python AggregateVAE/scripts/training/recons_grid_eval_tabular.py --script AggregateVAE/scripts/training/recons/tabular.py --dataset bankbalance --datastore ds/bank/balance --outstore train/bank/aggr --batch_size 8 --gpus 1')
 if __name__ == '__main__':
     Fire(main)
